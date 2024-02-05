4e9cbb1a0d7294106f826401b797e1e66fad1674
dmc mcc <mccarthydonal@hotmail.com>
2024-02-05 18:24:35 +0000

    basic fetch from trivia api

---
+++ bak.js
@@ -0,0 +1,21 @@
+<script>
+ 
+    //   fetch("https://the-trivia-api.com/v2/questions")
+    //fetch("https://ci-swapi.herokuapp.com/api/")
+    fetch("https://the-trivia-api.com/v2/session")
+        .then(response => response.text())
+        .then(data => displayData(data))
+        /*  {
+             document.getElementById("content").innerText = data.people;
+         })
+         */
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
+    function displayData(data) {
+        document.getElementById("content").innerText = data;
+        //document.getElementById("content").json = data.people;
+        //console.log(data.people)
+    }
+</script>
